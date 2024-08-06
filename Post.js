c16f6dc32b05c6d4d304af80a0db45618370bcb2
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-06 17:09:55 +0300

    All done

---
+++ Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  unpdatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
